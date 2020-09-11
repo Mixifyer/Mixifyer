3aba472166214b2b79f06c8692877fe6181737c3
Iskak Mantyubetov <ayzik55@gmail.com>
2020-09-11 16:52:38 -0400

    F: Admin access was created

--- server/api/permission.js
+++ server/api/permission.js
@@ -2,7 +2,7 @@ const isAdmin = (req, res, next) => {
   if (req.user && req.user.isAdmin) {
     next()
   } else {
-    const err = new Error('You do not have permission')
+    const err = new Error('You do not have permission!')
     err.status = 401
     return next(err)
   }
--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+const {isAdmin} = require('./permission')
 module.exports = router
 
-router.get('/', async (req, res, next) => {
+router.get('/', isAdmin, async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
@@ -13,5 +14,28 @@ router.get('/', async (req, res, next) => {
     res.json(users)
   } catch (err) {
     next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const user = await User.findByPk(req.params.id, {attributes: ['id']})
+    // explicitly select only the id and email fields - even though
+    // users' passwords are encrypted, it won't help if we just
+    // send everything to anyone who asks
+    res.json(user)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const removedUser = await User.findByPk(req.params.id, {attributes: ['id']})
+    await removedUser.destroy()
+    res.status(204).json('Deleted User')
+  } catch (error) {
+    console.log(error)
+    next(error)
   }
 })
