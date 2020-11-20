54d843800a1d13ac7c013d280cfcb499d5052926
Iskak Mantyubetov <ayzik55@gmail.com>
2020-11-20 11:50:53 -0500

    shoppingCart.js is done for store

--- client/shoppingCart.js
+++ client/shoppingCart.js
@@ -1,15 +1,57 @@
 import axios from 'axios'
 
 // ACTION TYPES:
-const GET_OR_UPDATE_CART = 'GET_CART'
-const REMOVE_PRODUCT_FROM_CARD = 'REMOVE_PRODUCT_FROM_CARD'
+const GET_OR_UPDATE_SHOPPINGCART = 'GET_OR_UPDATE_SHOPPINGCART'
+const REMOVE_PRODUCT_FROM_SHOPPINGCART = 'REMOVE_PRODUCT_FROM_SHOPPINGCART'
 
 // ACTION CREATORS:
-
-const getOrUpdateCart = shoppingCart => ({type: GET_CART, shoppingCart})
-const removeProductFromCart = productId => ({
-  type: REMOVE_PRODUCT_FROM_CARD,
+const getOrUpdateShoppingCart = shoppingCart => ({
+  type: GET_OR_UPDATE_SHOPPINGCART,
+  shoppingCart
+})
+const removeProductFromShoppingCart = productId => ({
+  type: REMOVE_PRODUCT_FROM_SHOPPINGCART,
   productId
 })
 
-// THUNKS
+// THUNKS:
+export const getShoppingCartOrCheckoutThunk = (
+  order,
+  method
+) => async dispatch => {
+  try {
+    if (method === 'checkout') {
+      const {data} = await axios.get('api/cart', order)
+      dispatch(getOrUpdateShoppingCart(data))
+    } else {
+      const {data} = await axios.get('api/cart')
+      dispatch(getOrUpdateShoppingCart(data))
+    }
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const updateShoppingCartThunk = (product, method) => async dispatch => {
+  try {
+    if (method === 'remove') {
+      const {data} = await axios.delete(`api/cart/${product.id}`)
+      dispatch(removeProductFromShoppingCart(product.id))
+    } else {
+      const {data} = await axios.put(`api/cart`, product)
+      dispatch(getOrUpdateShoppingCart(data))
+    }
+  } catch (error) {
+    console.error(error)
+  }
+}
+export default function(state, action) {
+  switch (action.type) {
+    case GET_OR_UPDATE_SHOPPINGCART:
+      return action.shoppingCart
+    case REMOVE_PRODUCT_FROM_SHOPPINGCART:
+      return state.filter(product => product.id === action.productId)
+    default:
+      return state
+  }
+}
