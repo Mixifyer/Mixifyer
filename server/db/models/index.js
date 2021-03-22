6791b53819f406470750b84a040989c77c2161b1
Iskak Mantyubetov <ayzik55@gmail.com>
2021-03-21 23:13:37 -0400

    added shoppingCartIcon component

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -27,13 +27,6 @@ Order.belongsToMany(Product, {through: 'ordered_product'})
 Product.belongsToMany(Order, {through: 'ordered_product'})
 
 /**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
-/**
  * We'll export all of our models here, so that any time a module needs a model,
  * we can just require it from 'db/models'
  * for example, we can say: const {User} = require('../db/models')
